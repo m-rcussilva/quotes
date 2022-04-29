3e96b2070da20fb2c00ca32e734fee16cf108f26
marcusluis <marcusluis.mail@gmail.com>
2022-04-29 18:17:12 -0300

    Sign In form criado

---
+++ src/components/SignInForm/SignInForm.js
@@ -0,0 +1,32 @@
+import "./SignInForm.css";
+
+const SignInForm = ({ toggleModal }) => {
+    return (
+        <div className="SignInForm-overlay">
+            <form className="SignInForm">
+                <div className="SignInForm-control">
+                    <label className="label">Email</label>
+                    <input type="email" className="input" />
+                </div>
+
+                <div className="SignInForm-control">
+                    <label className="label">Senha</label>
+                    <input type="password" className="input" />
+                </div>
+
+                <div className="SignInForm-container-btn">
+                    <button className="SignInForm-btn">Log In</button>
+                </div>
+
+                <button
+                    className="SignInForm-close-modal"
+                    onClick={toggleModal}
+                >
+                    X
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default SignInForm;
--- src/pages/Welcome/Welcome.js
+++ src/pages/Welcome/Welcome.js
@@ -1,33 +1,42 @@
+import { useState } from "react";
 import WelcomeAsideImg from "../../assets/images/welcome-aside-img.png";
+import SignInForm from "../../components/SignInForm/SignInForm";
 import "./Welcome.css";
 
 const Welcome = () => {
+    const [modal, setModal] = useState(false);
+
+    const toggleModal = () => {
+        setModal(!modal);
+    };
+
     return (
-        <div className="Welcome">
-            <div className="Welcome-container-left">
-                <h2 className="Welcome-text">
-                    Welcome to <span>Quotes</span>. A website where you can save
-                    your favorites quotes.
-                </h2>
+        <>
+            <div className="Welcome">
+                <div className="Welcome-container-left">
+                    <h2 className="Welcome-text">
+                        Welcome to <span>Quotes</span>. A website where you can
+                        save your favorites quotes.
+                    </h2>
 
-                <div className="Welcome-container-btn">
-                    <button className="Welcome-btn" id="Welcome-btn-signIn">
-                        Sign In
-                    </button>
-                    <button className="Welcome-btn" id="Welcome-btn-signUp">
-                        Sign Up
-                    </button>
+                    <div className="Welcome-container-btn">
+                        <button className="Welcome-btn" onClick={toggleModal}>
+                            Sign In
+                        </button>
+                    </div>
                 </div>
-            </div>
 
-            <div className="Welcome-container-right">
-                <img
-                    src={WelcomeAsideImg}
-                    alt="A woman reading a book"
-                    className="Welcome-aside-img"
-                />
+                <div className="Welcome-container-right">
+                    <img
+                        src={WelcomeAsideImg}
+                        alt="A woman reading a book"
+                        className="Welcome-aside-img"
+                    />
+                </div>
+
+                {modal && <SignInForm toggleModal={toggleModal} />}
             </div>
-        </div>
+        </>
     );
 };
 
